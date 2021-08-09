f754361a2931fb8a5c1ca3ec8df99227a4f1c60c
Crowdbotics <team@crowdbotics.com>
2021-08-09 23:18:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "long_snow_29679Identifier",
+  url: "https://long_snow_29679Identifier.botics.co",
+  api: "https://long_snow_29679Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
